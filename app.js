80ada58152470110ef5d0f9582ba948db22f4bf8
Yehor Lashchenko <lashchenko.ys@gmail.com>
2021-07-26 16:43:31 +0300

    the login page is finished

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+
+// const btn = document.querySelectorAll(".myBtn");
+
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
