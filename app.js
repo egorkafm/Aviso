d789e4e1fc26b616abeb0c25efb4fbee0be0ede5
Yehor Lashchenko <lashchenko.ys@gmail.com>
2021-07-27 16:51:36 +0300

    changed styles

--- app.js
+++ app.js
@@ -6,29 +6,26 @@
 
 let menuBtn = document.querySelector('.burger__menu-btn');
 let menu = document.querySelector('.burger__menu');
+const link = document.getElementById('headerLogin');
+const loginActive = document.querySelector(".header__login");
+const span = document.getElementsByClassName("close")[0];
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
-})
+});
 
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
 
-// const btn = document.querySelectorAll(".myBtn");
+link.addEventListener('click', function(){
+  loginActive.style.display = "block";
+});
 
-const span = document.getElementsByClassName("close")[0];
+span.addEventListener('click', function() {
+  loginActive.style.display = "none";
+});
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener('click', function(event) {
+  if (event.target == loginActive) {
+    loginActive.style.display = "none";
   }
-}
+})
---
+++ login.js
@@ -0,0 +1,39 @@
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+
+const page = document.querySelector(".page");
+// const page = document.getElementsByClassName("page");
+
+const exitModal = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// btn.addEventListener('click', function() {
+//   modal.style.display = "block";
+// });
+
+page.addEventListener('click', function() {
+  modal.style.display = "block";
+});
+
+// exitModal.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+exitModal.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+window.addEventListener('click', function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+})
